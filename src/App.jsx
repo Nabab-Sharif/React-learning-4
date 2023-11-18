79f4492dde5213ca683ff01ea2b9b8ce8e3cc496
md nabab sharif <mdnababsharifdiu@gmail.com>
2023-11-18 09:29:39 +0600

    add delete option done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,85 @@
-
+import { useEffect, useState } from "react"
+import { getDatabase, onValue, push, ref, set, remove } from "firebase/database"
 
 const App = () => {
+
+  const db = getDatabase();
+
+
+  let [text, setText] = useState("");
+  let [todo, setTodo] = useState([]);
+
+  let handleText = (e) => {
+    setText(e.target.value)
+
+  }
+
+  let handleSubmit = () => {
+    set(push(ref(db, "alltodo")), {
+      todotext: text,
+    })
+  }
+
+
+  useEffect(() => {
+    const todoRef = ref(db, "alltodo");
+    onValue(todoRef, (snapshot) => {
+
+      let arr = [];
+
+      snapshot.forEach((item) => {
+        arr.push({ ...item.val(), id: item.key })
+      })
+
+      setTodo(arr)
+
+    });
+  }, [])
+
+
+  let handleDelete = (id) => {
+    remove(ref(db, "alltodo/" + id)).then(() => {
+      alert("deleted")
+    })
+  }
+
+
+  let handleEdit = (id) => {
+    console.log(id)
+  }
+
+
+
+
   return (
     <>
-      <h2>CRUD App</h2>
+      <div className="todoContent">
+        <h2>Todo App</h2>
+        <div>
+          <input onChange={handleText} type="text" placeholder="Enter Your Task" />
+          <button className="btn" onClick={handleSubmit}>Add Task</button>
+        </div>
+
+        <div>
+          <ul>
+            {todo.length > 0
+              ?
+              todo.map((item, index) => (
+                <li key={index}>{item.todotext}
+
+                  <button className="btn" onClick={() => handleDelete(item.id)}>Delete</button>
+
+                  <button className="btn" onClick={() => handleEdit(item.id)}>Edit</button>
+
+                </li>
+              ))
+              :
+              <h1>Loading</h1>
+            }
+          </ul>
+        </div>
+
+      </div>
     </>
   )
 }
---
+++ src/configuration/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuDX7T_n8cusAAAZ0swHlvmx_Eb4OE0ZI",
+  authDomain: "realtime-todo-ccdf0.firebaseapp.com",
+  projectId: "realtime-todo-ccdf0",
+  storageBucket: "realtime-todo-ccdf0.appspot.com",
+  messagingSenderId: "745078535284",
+  appId: "1:745078535284:web:b6431ca7097ffb265c8d21"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
